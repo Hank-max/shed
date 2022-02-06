26dc2a325e6e0137de37d7edde70e636e2753c90
Hank-max <76566040+Hank-max@users.noreply.github.com>
2022-02-06 14:04:14 -0500

    Update window_motor_.ino

--- window_motor_/window_motor_.ino
+++ window_motor_/window_motor_.ino
@@ -3,8 +3,8 @@
 #define dirPin 26
 #define stepPin 28
 #define win_relay 36
-#define op_switch 25
-#define cl_switch 29
+#define op_switch 29
+#define cl_switch 25
 #define sensor1 3 //this is the sensor inside of the pi case
 #define stepsPerRevolution 1600*2
 long win_time = 15000;
@@ -13,6 +13,12 @@ unsigned long win_previousMillis = 0;
 #include "DHT.h"
 #define DHTTYPE DHT11 //leaving this here because i may get the DHT22
 DHT dht1(sensor1, DHTTYPE);//this is the sensor inside of the pi case
+
+
+int op_lastButtonState;   // the previous reading from the input pin
+int cl_lastButtonState;   // the previous reading from the input pin
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
 void setup() {
   // Declare pins as output:
@@ -36,48 +42,58 @@ void loop() {
   float tempF1 = dht1.readTemperature(true);// this is the sensor inside the pi case
   int  close_value = digitalRead(cl_switch);
   int open_value = digitalRead(op_switch);
+  Serial.print("Close ");Serial.print(close_value); Serial.print("     Open "   );Serial.println(open_value);
+  delay(100);
 
-
-
-  //Serial.print("temp    ");
-  //Serial.print(tempF1);
-  //Serial.print("    Close ");
-  //Serial.print(close_value);
-  //Serial.print("    Open ");
-  //Serial.println(open_value);
-  //
-  //delay(1000);
-  //&&
-
+  /*
+     Window is going from CLOSE to OPEN
+  */
   if ((tempF1 > 60) && (currentMillis - win_previousMillis >= win_time)) {
-    digitalWrite(win_relay, LOW);
-    delay(100);//this is to give the relay time to turn on/off.
-    if ((close_value == HIGH) && (open_value == LOW)) {
+    op_lastButtonState = LOW;
+    if ( open_value != op_lastButtonState) {
+      lastDebounceTime = currentMillis;
+    }
+    if ((open_value == LOW) && (close_value == HIGH) && ((currentMillis - lastDebounceTime) > debounceDelay)) {
+      digitalWrite(win_relay, LOW);
       digitalWrite(dirPin, HIGH);// this is setting the direction
-      for (int i = 0; i < stepsPerRevolution; i++) {
+      delay(100);//this is to give the relay time to turn on/off.
+
+      while (open_value == LOW) {
         digitalWrite(stepPin, HIGH);
-        delayMicroseconds(1000);
+        delayMicroseconds(500);
         digitalWrite(stepPin, LOW);
-        delayMicroseconds(1000);
+        delayMicroseconds(500);
+        open_value = digitalRead(op_switch);
       }
       win_previousMillis = currentMillis;
       delay(100);
       digitalWrite(win_relay, HIGH);
     }
+  }
 
-    else if ((close_value == LOW) && (open_value == HIGH)) {
-      digitalWrite(dirPin, LOW);
-      delay(100);
-      for (int i = 0; i < stepsPerRevolution; i++) {
+  /*
+     Window is going from OPEN to CLOSE
+  */
+  if ((tempF1 < 60) && (currentMillis - win_previousMillis >= win_time)) {
+    cl_lastButtonState = LOW;
+    if ( close_value != cl_lastButtonState) {
+      lastDebounceTime = currentMillis;
+    }
+    if ((open_value == HIGH) && (close_value == LOW) && ((currentMillis - lastDebounceTime) > debounceDelay)) {
+      digitalWrite(win_relay, LOW);
+      digitalWrite(dirPin, LOW);// this is setting the direction
+      delay(100);//this is to give the relay time to turn on/off.
+
+      while (close_value == LOW) {
         digitalWrite(stepPin, HIGH);
-        delayMicroseconds(1000);
+        delayMicroseconds(500);
         digitalWrite(stepPin, LOW);
-        delayMicroseconds(1000);
+        delayMicroseconds(500);
+        close_value = digitalRead(cl_switch);
       }
       win_previousMillis = currentMillis;
       delay(100);
       digitalWrite(win_relay, HIGH);
-      Serial.println("   else");
     }
   }
 }
