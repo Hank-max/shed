d299252d72275d1e20834d50bbeb45fa01b39057
Hank-max <76566040+Hank-max@users.noreply.github.com>
2022-02-14 18:54:07 -0500

    Update shed_non_qual_chopped.ino
    added the DHT sensor to the chopped code

--- shed_non_qual_chopped/shed_non_qual_chopped.ino
+++ shed_non_qual_chopped/shed_non_qual_chopped.ino
@@ -1,6 +1,15 @@
 
 
 /**********************************************************************************************
+   INT Relays
+***********************************************************************************************/
+const int dk_lights = 33; // 12V relay to lights in dog kennel.
+const int dk_fans = 43; //SS relay for fans in dog kennel.
+const int comp_fans = 34;//12V relay for fans in computer box.
+const int shed_fan = 47; // SS relay for shed fan.
+const int heater = 39; //SS relay for IR  Heater.
+
+/**********************************************************************************************
    INT Loop Times
 ***********************************************************************************************/
 const unsigned long SECONDS = 1000;
@@ -34,15 +43,43 @@ DeviceAddress shed_temp = {0x28, 0xD6, 0xE4, 0x10, 0x4C, 0x20, 0x01, 0x80 };// T
 DeviceAddress jango_temp = {0x28, 0xE7, 0xDF, 0xE2, 0x4B, 0x20, 0x1, 0xA2};// this is Jango
 //DeviceAddress relay_temp = { 0x28, 0x21, 0xB3, 0x95, 0xF0, 0x01, 0x3C, 0xAA };// Need to get the info for this sensor.
 
+/************************************************************************************************
+  INT DHT11 temp and humidity sensor
+ ***********************************************************************************************/
+#include "DHT.h"
+#define DHTTYPE DHT11 //type of sensor (may get DHT22 in future)
+#define sensor1 3 //Sensor inside of the pi case (~ Pi/Arduino temp)
+#define sensor2 4 //Sensor outside of the pi case (~shed temp)
+DHT dht1(sensor1, DHTTYPE);
+DHT dht2(sensor2, DHTTYPE);
 
+/*********************************************************************************************************************
+*********************************************************************************************************************
+                                SETUP
+*********************************************************************************************************************
+*********************************************************************************************************************/
 void setup()
 {
   Serial.begin(9600);
 
  sensors.begin(); // this is for the DS18B20 (oneWire)
+ 
+     dht1.begin();//DHT11 Temperature Sensors inside pelican case
+  dht2.begin();//DHT11 Temperature Sensors outside pelican case
 }
+/*********************************************************************************************************************
+*********************************************************************************************************************
+                                LOOP
+*********************************************************************************************************************
+*********************************************************************************************************************/
 void loop() {
   unsigned long currentMillis = millis();
+  
+  float hum1 = dht1.readHumidity();
+  float hum2 = dht2.readHumidity();
+  float tempF1 = dht1.readTemperature(true);//DHT11 Temperature Sensors inside pelican case
+  float tempF2 = dht2.readTemperature(true);//DHT11 Temperature Sensors outside pelican case
+  
   //Temp Sensor DS18B20
   sensors.requestTemperatures(); // Send the command to get temperatures
   float cricket_var = sensors.getTempF(cricket_temp);
@@ -57,7 +94,7 @@ void loop() {
     Serial Print
   ***********************************************************************************************************/
 if ((currentMillis - print_previousMillis) >= print_time) {
-    Serial.print("Shed Temp: "); Serial.print(shed_var); //Serial.print("   Comp Temp: "); Serial.print(tempF1);
+    Serial.print("Shed Temp: "); Serial.print(shed_var); Serial.print("   Comp Temp: "); Serial.print(tempF1);
     Serial.print("  Cricket Temp: "); Serial.print(cricket_var); Serial.print("  Jango Temp: "); Serial.print(jango_var);
     //Serial.print("  Relay Temp:"); Serial.print(relay_var);
     Serial.print("  Outside temp:"); Serial.print(outside_var);
