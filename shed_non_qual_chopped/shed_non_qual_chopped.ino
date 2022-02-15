eac976028ae4d9050b781d7bd13d87c6af4447ee
Hank-max <76566040+Hank-max@users.noreply.github.com>
2022-02-14 19:41:21 -0500

    Update shed_non_qual_chopped.ino
    added the data shield to the chopped script . The ones thing that may have been messed up in the printMillis = currentMillis was in the middle of the print and data loop.

--- shed_non_qual_chopped/shed_non_qual_chopped.ino
+++ shed_non_qual_chopped/shed_non_qual_chopped.ino
@@ -29,6 +29,35 @@ long fan_time = 5 * MIN;
 long print_time = 5 * SECONDS;
 long light_time = 5 * MIN;
 
+/**********************************************************************************************/
+/*INT Adafruit Data Shield
+ *********************************************************************************************
+   SD card attached to SPI bus as follows:
+ ** Mega:  MOSI - pin 51, MISO - pin 50, CLK - pin 52, CS - pin 4 (CS pin can be changed)
+  and pin #52 (SS) must be an output*/
+const int chipSelect = 10; // this is the CS for the data logger
+#include <SPI.h>
+#include <Wire.h> //This is the library that helps the Arduino with i2c
+#include "SD.h" // This is the library for talking to the card
+File dataFile;
+
+//******************* I need to read up on this and see if this can go in the setup. I dont want the system to stop if there is something wrong with the data logger.***********
+//#define LOG_INTEVAL 1000 //This is how many milli seconds between the sensor readings (i woul like to figure out these features, but currently not using.
+//#define ECHO_TO_SERIAL 1 //this is if i want to echo to the serial monitor (1 is on and 0 is off)
+void error(char *str)// the error function is uded if there is something bad happening (i.e. no micro SD card is installed. It will sit in the "while(1) loop forever until the issue is corrected
+{
+  Serial.print("error: ");
+  Serial.println(str);
+
+  //while (1);
+}
+
+/*********************************************************************************************
+   INT Time Stamp
+ * ******************************************************************************************/
+#include "RTClib.h" //this is the library the Arduino uses to chat with the real time clock
+RTC_PCF8523 rtc; // I found a different variable (RTC_DS1307) If the time is incorrect than i want to try this
+
 /***************************************************************************************************
   INT TSL2591 Digital Light Sensor
 ***************************************************************************************************/
@@ -88,6 +117,59 @@ void setup()
   dht2.begin();//DHT11 Temperature Sensors outside pelican case
   
   bme.begin();// BME280 Temp Sensor
+  
+    /**************************************************************************************************
+    SETUP Data Logger Shield Setup
+  *************************************************************************************************/
+  Serial.print("Initializing SD card...");
+  // make sure that the default chip select pin is set to
+  // output, even if you don't use it:
+  pinMode(chipSelect, OUTPUT);
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("SDCard failed, or not present");
+    // don't do anything more:
+    return ;
+  }
+
+  // Open up the file we're going to log to!
+  char filename[] = "LOGGER00.CSV";
+  for (uint8_t i = 0; i < 100; i++) {
+    filename[6] = i / 10 + '0';
+    filename[7] = i % 10 + '0';
+    if (! SD.exists(filename)) {
+      // only open a new file if it doesn't exist
+      dataFile = SD.open(filename, FILE_WRITE);
+      break;  // leave the loop!
+    }
+  }
+  dataFile = SD.open("datalog.csv", FILE_WRITE);
+  if (! dataFile) {
+    Serial.println("error creating file, SD may be full");
+  }
+  Serial.print("Logging to: ");
+  Serial.println(filename);
+
+  /**************************************************************************************************
+    SETUP Data Logger Shield Setup
+  *************************************************************************************************/
+  Wire.begin(); // i added this, so lets see if it works.
+  //    if (! rtc.begin()) {
+  //      Serial.println("Couldn't find RTC");
+  //      while (1);
+  //    }
+
+  dataFile.print("date/time"); // this is going to be the top column of the excel file, so adjust accordingly
+  if (! rtc.initialized()) {
+    Serial.println("RTC is NOT running!");
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    //rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }  
+  delay(100);// there may need to be a slight delay here for the sensor to 'warm up'
 }
 /*********************************************************************************************************************
 *********************************************************************************************************************
@@ -148,6 +230,11 @@ void loop() {
     Serial Print
   ***********************************************************************************************************/
   if ((currentMillis - print_previousMillis) >= print_time) {
+	  
+    DateTime now = rtc.now();
+    Serial.print(now.year(), DEC); Serial.print('/'); Serial.print(now.month(), DEC); Serial.print('/');
+    Serial.print(now.day(), DEC); Serial.print(") "); Serial.print(now.hour(), DEC); Serial.print(':');
+    Serial.print(now.minute(), DEC); Serial.print(':'); Serial.println(now.second(), DEC);
     Serial.print("Shed Temp: "); Serial.print(shed_var); Serial.print("   Comp Temp: "); Serial.print(tempF1);
     Serial.print("  Cricket Temp: "); Serial.print(cricket_var); Serial.print("  Jango Temp: "); Serial.print(jango_var);
     //Serial.print("  Relay Temp:"); Serial.print(relay_var);
@@ -158,6 +245,17 @@ void loop() {
 	
 	    Serial.print("Dog Kennel AVG:"); Serial.print(dk_avg); Serial.print("  Outside AVG:"); Serial.print(outside_avg);
     Serial.print("  Shed AVG:"); Serial.println(shed_avg);
+	
+    /***********************************************************************************************************
+//      Data File Print
+//    ***********************************************************************************************************/
+    dataFile.print(now.year(), DEC); dataFile.print('/'); dataFile.print(now.month(), DEC); dataFile.print('/');
+    dataFile.print(now.day(), DEC); dataFile.print(") "); dataFile.print(","); dataFile.print(now.hour(), DEC);
+    dataFile.print(':'); dataFile.print(now.minute(), DEC); dataFile.print(':'); dataFile.print(now.second(), DEC);
+    dataFile.print(",");
+
+    dataFile.print("Light Sensor: ir full full-ir lux"); dataFile.print(","); dataFile.print(ir); dataFile.print(",");
+    dataFile.print(full); dataFile.print(","); dataFile.print(full - ir); dataFile.print(","); dataFile.print(tsl.calculateLux(full, ir), 6);
 	
 	print_previousMillis = currentMillis;
 }
